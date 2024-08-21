b12ddb0ee238fc7ca82e125175b58377516bd83c
SappyDude <dhystymortal@gmail.com>
2024-08-21 14:27:59 -0500

    cambio

---
+++ Java universidad/2_ejercicios/1_condicionales_dobles/src/main/java/com/mycompany/_condicionales_dobles/App.java
@@ -0,0 +1,9 @@
+package com.mycompany._condicionales_dobles;
+
+/*@author dhysty */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Java universidad/2_ejercicios/1_condicionales_dobles/src/main/java/com/mycompany/_condicionales_dobles/Codigo.java
@@ -0,0 +1,10 @@
+package com.mycompany._condicionales_dobles;
+import java.util.
+/*@author dhysty*/
+
+public class Codigo {
+    float co1, co2, condicion, r1, r2, img;
+    public Codigo() {        
+        
+    }
+}

ead0d9a56d1672cc111e0e8a1395b63733c50a54
SappyDude <dhystymortal@gmail.com>
2024-08-21 16:10:47 -0500

    ifelse

--- PROGRAMACIÓN/Java universidad/2_ejercicios/Propios/2_Condicionales_dobles_java/src/pkg3_condicionales_dobles_java/Main.java
+++ PROGRAMACIÓN/Java universidad/2_ejercicios/Propios/2_Condicionales_dobles_java/src/pkg3_condicionales_dobles_java/Main.java
@@ -4,19 +4,28 @@ import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
             Scanner Leer = new Scanner (System.in);
-            
             int co1, co2, co3, condition, r1, r2;
+            //Solo funciona con enteros!!!
             System.out.println("Ingrese el primer cociente de su cuadrática");
             co1 = Leer.nextInt();
+            
             System.out.println("Ingrese el segundo cociente de su cuadrática");
             co2 = Leer.nextInt();
+            
             System.out.println("Ingrese el tercer cociente de su cuadrática");
             co3 = Leer.nextInt();
-            if ((co2*co2)-(4*(co1)*(co2)) >= 0) {
-                System.out.println("");
+            
+            condition = (co2 * co2) - (4 * (co1) * (co3));
+            
+            if (condition >= 0) {
+                r1 = ((-co2) + ((co2 * co2) - (condition))^(1/2)) / (2 * co1);
+                r2 = ((-co2) - ((co2 * co2) - (condition))^(1/2)) / (2 * co1);
+                System.out.println("sus raices son: " + r1 + " y " + r2);
             } else {
                 System.out.println("Sus raices son imaginarias");
+                
             }
+            
     }
     
 }

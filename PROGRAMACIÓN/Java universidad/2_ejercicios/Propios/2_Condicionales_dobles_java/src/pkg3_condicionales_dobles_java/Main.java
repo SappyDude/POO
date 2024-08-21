28a3f6b31338057189ecd9514af2fcb7e8286ab2
SappyDude <dhystymortal@gmail.com>
2024-08-21 15:54:29 -0500

    codigo raices imaginarias

--- PROGRAMACIÓN/Java universidad/2_ejercicios/Propios/2_Condicionales_dobles_java/src/pkg3_condicionales_dobles_java/Main.java
+++ PROGRAMACIÓN/Java universidad/2_ejercicios/Propios/2_Condicionales_dobles_java/src/pkg3_condicionales_dobles_java/Main.java
@@ -3,7 +3,20 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-        // TODO code application logic here
+            Scanner Leer = new Scanner (System.in);
+            
+            int co1, co2, co3, condition, r1, r2;
+            System.out.println("Ingrese el primer cociente de su cuadrática");
+            co1 = Leer.nextInt();
+            System.out.println("Ingrese el segundo cociente de su cuadrática");
+            co2 = Leer.nextInt();
+            System.out.println("Ingrese el tercer cociente de su cuadrática");
+            co3 = Leer.nextInt();
+            if ((co2*co2)-(4*(co1)*(co2)) >= 0) {
+                System.out.println("");
+            } else {
+                System.out.println("Sus raices son imaginarias");
+            }
     }
     
 }

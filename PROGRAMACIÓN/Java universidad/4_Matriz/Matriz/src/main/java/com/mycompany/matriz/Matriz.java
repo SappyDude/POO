9ceb5d09fef7817f33f720743172bac1b8fcdeb8
SappyDude <dhystymortal@gmail.com>
2024-08-27 12:45:02 -0500

    Clase matrices

---
+++ PROGRAMACIÓN/Java universidad/4_Matriz/Matriz/src/main/java/com/mycompany/matriz/Matriz.java
@@ -0,0 +1,33 @@
+package com.mycompany.matriz;
+
+public class Matriz {
+
+    public static void main(String[] args) {
+        //Declarar
+        int edades[][];
+
+        //Instanciar, es crear un espacio en la memoria
+        edades = new int[3][2];
+
+        //declarar, instanciarse, iniciar
+        edades[0][0] = 30;
+        edades[0][1] = 15;
+        edades[1][0] = 20;
+        edades[1][1] = 45;
+        edades[2][0] = 5;
+        edades[2][0] = 1;
+        //aqui se declara y se instancia al mismo tiempo
+        Persona personas [][] = new Persona [2][3];
+        //se le designa un nuevo objeto al constructor
+        personas[0][0] = new Persona("Juan");
+        
+        //Imprimir
+        //recorre las casillas
+        for (int i = 0; i < edades.length; i++) {
+            for (int j = 0; j < edades[i].length; j++) {
+                System.out.println("Matriz edades " + i + " , " + j + ": " + edades[i][j]);
+            }
+        }
+        
+    }
+}
---
+++ PROGRAMACIÓN/Java universidad/4_Matriz/Matriz/src/main/java/com/mycompany/matriz/Persona.java
@@ -0,0 +1,34 @@
+package com.mycompany.matriz;
+
+public class Persona {
+    
+//Atributos
+    private String nombre;
+    private int edad;
+    
+    
+    
+    
+//Constructor
+    public Persona (String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+   
+    
+//Métodos    
+    public String getNombre() {
+        return nombre;
+        
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    
+    
+    
+        
+}

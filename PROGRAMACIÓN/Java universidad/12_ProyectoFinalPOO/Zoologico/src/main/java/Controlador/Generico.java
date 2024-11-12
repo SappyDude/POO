fe9614bbd67fe56c5688d2b42c6a1ee7a5035493
SappyDude <dhystymortal@gmail.com>
2024-11-11 23:56:35 -0500

    documentos del proyecto final v1.0

---
+++ PROGRAMACIÓN/Java universidad/12_ProyectoFinalPOO/Zoologico/src/main/java/Controlador/Generico.java
@@ -0,0 +1,10 @@
+
+package Controlador;
+
+/**
+ *
+ * @author SappyDude
+ */
+public class Generico {
+    
+}
---
+++ PROGRAMACIÓN/Java universidad/12_ProyectoFinalPOO/cursoTries/src/main/java/com/mycompany/cursotries/CursoTries.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cursotries;
+
+/**
+ *
+ * @author SappyDude
+ */
+public class CursoTries {
+
+    public static void main(String[] args) {
+        var salida = "come";
+        System.out.println(salida );
+    }
+}
